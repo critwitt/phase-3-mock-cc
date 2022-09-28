0e894704a88c31935b4ffee11f4aa0f84396c383
Chris Witte <christopher.t.witte@gmail.com>
2022-09-28 11:22:48 -0400

    completed mock cc

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,13 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(dev_id: 9, company_id: 9, item_name: 'Frisbee', value: 5)
+Freebie.create(dev_id: 9, company_id: 10, item_name: 'Pen', value: 2)
+Freebie.create(dev_id: 10, company_id: 11, item_name: 'Frisbee', value: 5)
+Freebie.create(dev_id: 10, company_id: 12, item_name: 'Hackey Sack', value: 3)
+Freebie.create(dev_id: 11, company_id: 10, item_name: 'Hat', value: 10)
+Freebie.create(dev_id: 11, company_id: 12, item_name: 'Sweatshirt', value: 15)
+Freebie.create(dev_id: 12, company_id: 9, item_name: 'Hat', value: 10)
+Freebie.create(dev_id: 12, company_id: 11, item_name: 'Bouncy Ball', value: 1)
 
 puts "Seeding done!"
